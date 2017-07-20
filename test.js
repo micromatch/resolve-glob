f13b2d584cc764c392a6b4869c0ac07c58701a2a
jonschlinkert <jon.schlinkert@sellside.com>
2017-07-20 17:27:18 -0400

    run update

--- test.js
+++ test.js
@@ -2,13 +2,12 @@
 
 require('mocha');
 var assert = require('assert');
-var should = require('should');
 var isAbsolute = require('is-absolute');
 var glob = require('./');
 
-describe('glob', function () {
-  it('should resolve the absolute path for files returned by glob:', function (cb) {
-    glob('*.js', function (err, files) {
+describe('glob', function() {
+  it('should resolve the absolute path for files returned by glob:', function(cb) {
+    glob('*.js', function(err, files) {
       if (err) return cb(err);
       assert(files.length > 0);
       assert(isAbsolute(files[0]));
@@ -16,8 +15,8 @@ describe('glob', function () {
     });
   });
 
-  it('should support arrays:', function (cb) {
-    glob(['*.js', '*.md', '!**/test.js'], function (err, files) {
+  it('should support arrays:', function(cb) {
+    glob(['*.js', '*.md', '!**/test.js'], function(err, files) {
       if (err) return cb(err);
       assert(files.length > 0);
       assert(isAbsolute(files[0]));
@@ -25,78 +24,69 @@ describe('glob', function () {
     });
   });
 
-  it('should resolve the absolute path with a cwd:', function (cb) {
+  it('should resolve the absolute path with a cwd:', function(cb) {
     var opts = {cwd: 'node_modules/is-absolute'};
-    glob('*.js', opts, function (err, files) {
+    glob('*.js', opts, function(err, files) {
       if (err) return cb(err);
       assert(files.length > 0);
       assert(isAbsolute(files[0]));
       cb();
-    })
+    });
   });
 
-  it('should resolve the absolute path to the user home directory:', function (cb) {
-    glob('*.*', {cwd: '~'}, function (err, files) {
+  it('should resolve the absolute path to the user home directory:', function(cb) {
+    glob('*.*', {cwd: '~'}, function(err, files) {
       if (err) return cb(err);
       assert(files.length > 0);
       assert(isAbsolute(files[0]));
       cb();
-    })
+    });
   });
 
-  it('should resolve the absolute path to global npm modules:', function (cb) {
-    glob('*', {cwd: '@'}, function (err, files) {
+  it('should resolve the absolute path to global npm modules:', function(cb) {
+    glob('*', {cwd: '@'}, function(err, files) {
       if (err) return cb(err);
       assert(files.length > 0);
       assert(isAbsolute(files[0]));
       cb();
-    })
+    });
   });
 
-  it('should throw an error when a callback is not passed:', function () {
-    try {
+  it('should throw an error when a callback is not passed:', function() {
+    assert.throws(function() {
       glob();
-      throw new Error('expected an error to be thrown');
-    } catch(err) {
-      assert(err);
-      assert(err.message === 'expected a callback function.');
-    }
+    });
   });
 
-  it('should throw an error when invalid args are passed:', function (cb) {
-    glob(null, function (err, files) {
+  it('should throw an error when invalid args are passed:', function(cb) {
+    glob(null, function(err, files) {
       assert(err);
-      assert(err.message === 'expected a valid glob pattern.');
       cb();
     });
   });
 });
 
-describe('glob.sync', function () {
-  it('should resolve the absolute path for files returned by glob:', function () {
+describe('glob.sync', function() {
+  it('should resolve the absolute path for files returned by glob:', function() {
     var files = glob.sync('*.js');
     assert(files.length > 0);
     assert(isAbsolute(files[0]));
   });
 
-  it('should return the pattern when no matches and nonull is passed:', function () {
+  it('should return the pattern when no matches and nonull is passed:', function() {
     var files = glob.sync('*.foo', {nonull: true});
     assert(files[0] === '*.foo');
   });
 
-  it('should resolve the absolute path with a cwd:', function () {
+  it('should resolve the absolute path with a cwd:', function() {
     var files = glob.sync('*.js', {cwd: 'node_modules/is-absolute'});
     assert(files.length > 0);
     assert(isAbsolute(files[0]));
   });
 
-  it('should throw an error when invalid args are passed:', function () {
-    try {
+  it('should throw an error when invalid args are passed:', function() {
+    assert.throws(function() {
       glob.sync();
-      throw new Error('expected an error to be thrown');
-    } catch(err) {
-      assert(err);
-      assert(err.message === 'expected a valid glob pattern.');
-    }
+    });
   });
 });
